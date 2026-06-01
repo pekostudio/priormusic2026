6937770b68c020d7ae5636ca304d780e34469f68
Andrius Pekarskas <hello@peko.lt>
2026-06-01 18:48:48 +0300

    Fix home route test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('returns a successful response', function () {
+test('home redirects to tracks', function () {
     $response = $this->get(route('home'));
 
-    $response->assertRedirect(route('login'));
+    $response->assertRedirect(route('tracks', absolute: false));
 });
